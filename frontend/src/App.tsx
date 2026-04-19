08dce4d4359b546a0b1b9cc911b8b131d5029fdd
Saurabh Chaubey <saurabhchaubey112@gmail.com>
2026-04-19 15:59:58 +0530

    home page with routing security implemented

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import Home from "./pages/Home";
+import Home from "./pages/Home";
 import Login from "./pages/Login";
-// import ProtectedRoute from "./components/protectedRoute";
+import ProtectedRoute from "./components/protectedRoute";
 import PublicRoute from "./components/publicRoute";
-// import SelectRole from "./pages/SelectRole";
-// import Navbar from "./components/navbar";
-// import Account from "./pages/Account";
+
 import { useAppData } from "./context/AppContext";
-// import Restaurant from "./pages/Restaurant";
-// import RestaurantPage from "./pages/RestaurantPage";
-// import Cart from "./pages/Cart";
-// import AddAddressPage from "./pages/Address";
-// import Checkout from "./pages/Checkout";
-// import PaymentSuccess from "./pages/PaymentSuccess";
-// import OrderSuccess from "./pages/OrderSuccess";
-// import Orders from "./pages/Orders";
-// import OrderPage from "./pages/OrderPage";
-// import RiderDashboard from "./pages/RiderDashboard";
-// import Admin from "./pages/Admin";
+
 
 const App = () => {
    const { user, loading } = useAppData();
@@ -26,17 +14,7 @@ const App = () => {
     return <h1 className="text-2xl font-bold text-red-500 text-center mt-56">Loading...</h1>
   }
 
-  // if (user && user.role === "seller") {
-  //   return <Restaurant />;
-  // }
-
-  // if (user && user.role === "rider") {
-  //   return <RiderDashboard />;
-  // }
-
-  // if (user && user.role === "admin") {
-  //   return <Admin />;
-  // }
+ 
 
   return (
     <>
@@ -47,19 +25,9 @@ const App = () => {
             <Route path="/login" element={<Login />} />
           </Route>
 
-          {/* <Route element={<ProtectedRoute />}>
+          <Route element={<ProtectedRoute />}>
             <Route path="/" element={<Home />} />
-            <Route path="/paymentsuccess/:paymentId" element={<PaymentSuccess />} />
-            <Route path="/ordersuccess" element={<OrderSuccess />} />
-            <Route path="/orders" element={<Orders />} />
-            <Route path="/order/:id" element={<OrderPage />} />
-            <Route path="/address" element={<AddAddressPage />} />
-            <Route path="/checkout" element={<Checkout />} />
-            <Route path="/restaurant/:id" element={<RestaurantPage />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/select-role" element={<SelectRole />} />
-            <Route path="/account" element={<Account />} />
-          </Route> */}
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
