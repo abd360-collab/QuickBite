c97b8047e25df72c3e190837e0311a2d8a134929
Saurabh Chaubey <saurabhchaubey112@gmail.com>
2026-05-17 09:51:25 +0530

    database connection before server starts

--- services/auth/src/index.ts
+++ services/auth/src/index.ts
@@ -16,7 +16,8 @@ app.use("/api/auth", authRoute);
 
 const PORT = process.env.PORT || 5000;
 
+connectDB();
+
 app.listen(PORT, () => {
     console.log(`Auth service is running on port ${PORT}`);
-    connectDB();
 });
